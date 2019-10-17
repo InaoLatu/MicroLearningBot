0d6ea4d6b83f8b3fa94ce794b1c26abe223e2618
InaoLatu <inaolat@gmail.com>
2019-10-17 12:21:20 +0200

    Managing of JSON data and quiz done.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import types
+
+import telegram
+from telegram import Message
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
 import re
@@ -11,32 +15,18 @@ TOKEN = "729590852:AAFHIQhSbUcLzXyXhh7ieaSheWtD1IU1wT0"
 URL_bot = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
-def bop(bot, update):
-    url = get_image_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
-
-def get_image_url():
-    allowed_extensions = ['jpg', 'png']
-    file_extension = ''
-    url = ''
-    while file_extension not in allowed_extensions:
-        contents = requests.get('https://random.dog/woof.json').json()
-        url = contents['url']
-        file_extension = re.search("([^.]*)$", url).group(1).lower()
-    return url
-
-
 def pic(bot, update):
     contents = requests.get('https://random-d.uk/api/v2/random').json()
     url = contents['url']
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
+
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
+
 
 def get_json_from_url(url):
     content = get_url(url)
@@ -51,11 +41,13 @@ def get_updates(offset=None):
     js = get_json_from_url(url)
     return js
 
+
 def get_last_update_id(updates):
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
+
 
 def send_message(text, chat_id, reply_markup=None):
     text = urllib.parse.quote_plus(text)
@@ -72,7 +64,6 @@ def handle_updates(updates, index, contents, chat_id, keyboard):
         send_message("Question " + str(index) + ": " + contents['quiz'][index]['question'], chat_id, keyboard)
 
 
-
 def build_keyboard(items):
     keyboard = [[item] for item in items]
     reply_markup = {"keyboard": keyboard, "one_time_keyboard": True}
@@ -87,9 +78,9 @@ def build_quiz(contents, bot, update, chat_id):
         index = 0
         while True:
             updates = get_updates(last_update_id)
+            time.sleep(1)
             if (len(updates["result"])) > 0:
                 last_update_id = get_last_update_id(updates) + 1
-                time.sleep(0.5)
                 if index+1 >= 3:
                     break
                 else:
@@ -97,18 +88,23 @@ def build_quiz(contents, bot, update, chat_id):
                     send_message("Question " + str(index) + ": " + contents['quiz'][index]['question'], chat_id, keyboard)
 
 
-
-
-def get_mc(bot, update):  #Get the micro content json
+def get_mc(bot, update):  # Get the micro content json
     contents = requests.get('http://178.79.170.232:8000/json?content=1').json()
     title = contents['title']
     author = contents['meta_data']['author']
     explanation = contents['text'][0]
 
     chat_id = update.message.chat_id
+    markup = telegram.ReplyKeyboardRemove(selective=False)  # To remove the keyboard buttons from the previous interaction
+    bot.send_message(chat_id, "Hi! You are going to start the micro content! ", reply_markup=markup)
     bot.send_message(chat_id=chat_id, text="Micro content: "+title)
+    #  bot.send_video(chat_id=chat_id, video="http://178.79.170.232:8000/micro_content_manager/static/videos/RecordRTC-2019914-6v15d09xs1f.webm")
+
+
     bot.send_message(chat_id=chat_id, text=explanation)
     bot.send_message(chat_id=chat_id, text="Author: "+author)
+    video = open('/home/inao/Trabajo/ElemendBot/media/videos/microlearning.mp4', 'rb')
+    bot.send_video(chat_id, video)
     build_quiz(contents, bot, update, chat_id)
 
 
@@ -116,10 +112,10 @@ def main():
     updater = Updater('729590852:AAFHIQhSbUcLzXyXhh7ieaSheWtD1IU1wT0')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('pic', pic))
-    dp.add_handler(CommandHandler('bop', bop))
     dp.add_handler(CommandHandler('mc', get_mc))
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
