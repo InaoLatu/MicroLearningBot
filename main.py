419ab5edbc008949fce35511bfed09729125f5e9
Mario Manso <brossanova@gmail.com>
2021-01-02 13:01:06 +0100

    Traducción

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ id_telegram_user = ""
 auth_username = ""  # Username previously registered in the Auth Server
 
 # General Manager API where all the request of the Bot are directed to
-GENERAL_MANAGER_IP = "http://192.168.0.26:7030/general_manager/"
+GENERAL_MANAGER_IP = "http://192.168.0.20:7030/general_manager/"
+AUTH_TOOL_UNITS_IP = 'http://192.168.0.20:7020/units'
 
 
 def start(update, context):
@@ -47,20 +48,20 @@ def start(update, context):
     global id_telegram_user
     id_telegram_user = update.effective_user.id
 
-    update.message.reply_text("Please introduce the following command to authenticate: /auth")
+    update.message.reply_text("Por favor, introduce el siguiente comando para acceder: /auth")
 
 
 def help(update, context):
-    update.message.reply_text("You have the following commands available:\n\n"
-                              "/help - Show the commands available.\n"
-                              "/menu - Show you the available Units.\n"
-                              "/cancel - Cancel the conversation.")
+    update.message.reply_text("Comandos disponibles:\n\n"
+                              "/help - Muestra los comandos disponibles\n"
+                              "/menu - Muestra la lista de unidades\n"
+                              "/cancel - Cancela la conversación")
 
 
 def cancel(update, context):
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('Bye! I hope we can talk again some day.',
+    update.message.reply_text('¡Hasta otra! Espero que hablemos pronto.',
                               reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
@@ -137,26 +138,26 @@ def end_quiz(update, context):
     # the selection of the user in this state. Each block include one question with the selection and the correct answer.
     for q in micro_content['quiz']:
         block = ""
-        question = "*Question " + str(index) + "*: " + micro_content['quiz'][index]['question'] + "\n"
+        question = "*Pregunta " + str(index) + "*: " + micro_content['quiz'][index]['question'] + "\n"
         question = question.upper()
-        selection = "You chose: " + selections[index] + "\n"
+        selection = "Has elegido: " + selections[index] + "\n"
         block = block + question + selection
         #MOD si no hay explicación, mostramos texto por defecto.
         mc_explanation = micro_content['quiz'][index]['explanation']
         if mc_explanation == '':
             mc_explanation = 'No disponible.'
 
-        explanation = "Explanation: " + mc_explanation
+        explanation = "Explicación: " + mc_explanation
         
         if micro_content['quiz'][index]['answer'] == selections[index]:
-            solution_message = "CORRECT! " + "\U0001F603" + "\n"
+            solution_message = "¡CORRECTO! " + "\U0001F603" + "\n"
             block = block + solution_message
             block = block + explanation
             correct_answers.append(block)
             correct = correct + 1
         else:
-            solution_message = "Ups, wrong " + "\U0001F641" + "\n"
-            correct_answer = "Correct answer: " + micro_content['quiz'][index]['answer'] + "\n"
+            solution_message = "Incorrecto " + "\U0001F641" + "\n"
+            correct_answer = "Respuesta correcta: " + micro_content['quiz'][index]['answer'] + "\n"
             block = block + solution_message + correct_answer
             block = block + explanation
             wrong_answers.append(block)
@@ -173,8 +174,8 @@ def end_quiz(update, context):
     }
     print(data)
     requests.post(request_string, data=data)
-    update.message.reply_text("CONGRATS! You have completed the micro content!")
-    update.message.reply_text("YOUR FINAL RESULT: " + str(correct) + "/" + str(len(micro_content['quiz'])))
+    update.message.reply_text("¡ENHORABUENA! Has completado el micro contenido!")
+    update.message.reply_text("TU RESULTADO FINAL: " + str(correct) + "/" + str(len(micro_content['quiz'])))
     selections.clear()
 
     context.user_data['correct_answers'] = correct_answers
@@ -182,13 +183,13 @@ def end_quiz(update, context):
     context.user_data['all_answers'] = all_answers
 
     button_list = [
-        InlineKeyboardButton("Only the wrong ones", callback_data="wrong"),
-        InlineKeyboardButton("Only the correct ones", callback_data="correct"),
-        InlineKeyboardButton("All", callback_data="all"),
-        InlineKeyboardButton("Back to Units", callback_data="back"),
+        InlineKeyboardButton("Sólo las incorrectas", callback_data="wrong"),
+        InlineKeyboardButton("Sólo las correctas", callback_data="correct"),
+        InlineKeyboardButton("Todas", callback_data="all"),
+        InlineKeyboardButton("Volver a Unidades", callback_data="back"),
     ]
     reply_markup = InlineKeyboardMarkup(utils.build_menu(button_list, n_cols=1))
-    update.message.reply_text(text="*Check your answers:* ",
+    update.message.reply_text(text="*Comprueba tus respuestas:* ",
                               parse_mode=telegram.ParseMode.MARKDOWN,
                               reply_markup=reply_markup)
 
@@ -202,7 +203,7 @@ def show_wrong_answers(update, context):
     bot.send_message(
         chat_id=query.message.chat_id,
         reply_to_message_id=query.message.message_id,
-        text="*WRONG ANSWERS:*",
+        text="*RESPUESTAS INCORRECTAS:*",
         parse_mode=telegram.ParseMode.MARKDOWN,
     )
 
@@ -220,8 +221,8 @@ def show_wrong_answers(update, context):
     bot.send_message(
         chat_id=query.message.chat_id,
         reply_to_message_id=query.message.message_id,
-        text="You can keep doing other micro content with the following command:\n\n"
-             "/menu - Show you the available Units.",
+        text="Puedes hacer otros micro contenidos con el siguiente comando:\n\n"
+             "/menu - Muestra la lista de unidades.",
         parse_mode=telegram.ParseMode.MARKDOWN,
     )
 
@@ -235,7 +236,7 @@ def show_correct_answers(update, context):
     bot.send_message(
         chat_id=query.message.chat_id,
         reply_to_message_id=query.message.message_id,
-        text="*CORRECT ANSWERS:*",
+        text="*RESPUESTAS CORRECTAS:*",
         parse_mode=telegram.ParseMode.MARKDOWN,
     )
 
@@ -251,8 +252,8 @@ def show_correct_answers(update, context):
     bot.send_message(
         chat_id=query.message.chat_id,
         reply_to_message_id=query.message.message_id,
-        text="You can keep doing other micro content with the following command:\n\n"
-             "/menu - Show you the available Units.",
+        text="Puedes hacer otros micro contenidos con el siguiente comando:\n\n"
+             "/menu - Muestra la lista de unidades.",
         parse_mode=telegram.ParseMode.MARKDOWN,
     )
 
@@ -267,7 +268,7 @@ def show_all_answers(update, context):
     bot.send_message(
         chat_id=query.message.chat_id,
         reply_to_message_id=query.message.message_id,
-        text="*ALL ANSWERS:*",
+        text="*TODAS LAS RESPUESTAS:*",
         parse_mode=telegram.ParseMode.MARKDOWN,
     )
 
@@ -283,8 +284,8 @@ def show_all_answers(update, context):
     bot.send_message(
         chat_id=query.message.chat_id,
         reply_to_message_id=query.message.message_id, #MOD: message_id not working (send_message changed). Now reply_to_...
-        text="You can keep doing other micro content with the following command:\n\n"
-             "/menu - Show you the available Units.",
+        text="Puedes hacer otros micro contenidos con el siguiente comando:\n\n"
+             "/menu - Muestra la lista de unidades.",
         parse_mode=telegram.ParseMode.MARKDOWN,
     )
 
@@ -298,7 +299,7 @@ def get_micro_content(update, context):  # Gets the selected micro-content
     bot.edit_message_text(
         chat_id=query.message.chat_id,
         message_id=query.message.message_id,
-        text="Start the micro content!",
+        text="¡Comienza el micro contenido!",
     )
 
     match = context.match
@@ -311,10 +312,10 @@ def get_micro_content(update, context):  # Gets the selected micro-content
     micro_content = requests.get(GENERAL_MANAGER_IP + 'microcontent?id=' + str(micro_content_id)).json()
 
     bot.send_message(chat_id,
-                     text="Micro content: " + micro_content['title'] + "\n" + micro_content['text'][0] + "\nAuthor: " +
+                     text="Micro contenido: " + micro_content['title'] + "\n" + micro_content['text'][0] + "\nAutor/a: " +
                           micro_content['meta_data']['author'])
 
-    bot.send_message(chat_id, text="Loading video...")
+    bot.send_message(chat_id, text="Cargando vídeo...")
     
     #MOD esto estaba hardcoded
     #video = open('/home/inao/Trabajo/ElemendBot/media/videos/microlearning.mp4', 'rb')#OJO CORREGIR!!!
@@ -332,11 +333,11 @@ def get_micro_content(update, context):  # Gets the selected micro-content
     #bot.send_video(chat_id, video, supports_streaming = TRUE)
     
     button_list = [
-        InlineKeyboardButton("Yes!", callback_data="yes"),
+        InlineKeyboardButton("¡Sí!", callback_data="yes"),
     ]
     reply_markup = InlineKeyboardMarkup(utils.build_menu(button_list, n_cols=1))
 
-    bot.send_message(chat_id, text="Ready for questions?", reply_markup=reply_markup)
+    bot.send_message(chat_id, text="¿Preparado para las preguntas?", reply_markup=reply_markup)
 
     return START_QUIZ
 
@@ -356,13 +357,13 @@ def get_unit_micro_contents(update, context):  # Get the micro-content included 
     button_list = []
     for mc in micro_content_list:
         if mc['completed']:
-            button_list += [InlineKeyboardButton(mc['title']+" - completed", callback_data=str(mc['micro_id']))] #TRANS
+            button_list += [InlineKeyboardButton(mc['title']+" - completado", callback_data=str(mc['micro_id']))] #TRANS
         else:
             button_list += [InlineKeyboardButton(mc['title'], callback_data=str(mc['micro_id']))]
         conv_handler.states[MENU2].append(
             CallbackQueryHandler(get_micro_content, pattern='^' + str(mc['micro_id']) + '$'))  # Update states
 
-    button_list += [InlineKeyboardButton("Back to Units", callback_data="back")] 
+    button_list += [InlineKeyboardButton("Volver a unidades", callback_data="back")] 
     #button_list += [InlineKeyboardButton(request_string, callback_data="back")]
 
     reply_markup = InlineKeyboardMarkup(utils.build_menu(button_list, n_cols=2))
@@ -370,7 +371,7 @@ def get_unit_micro_contents(update, context):  # Get the micro-content included 
     bot.edit_message_text(
         chat_id=query.message.chat_id,
         message_id=query.message.message_id,
-        text="*" + unit.capitalize() + "* \n" + "Choose a micro content: \n",
+        text="*" + unit.capitalize() + "* \n" + "Elige un micro contenido: \n",
         parse_mode=telegram.ParseMode.MARKDOWN,
         reply_markup=reply_markup
     )
@@ -387,7 +388,7 @@ def start_quiz(update, context):  # Shows the first question of the quiz and ret
 
     choices = micro_content['quiz'][0]['choices']
     reply_keyboard = build_keyboard([choices[0]['choice_text'], choices[1]['choice_text'], choices[2]['choice_text']])
-    bot.send_message(chat_id, text="Question " + str(1) + ": " + micro_content['quiz'][0]['question'],
+    bot.send_message(chat_id, text="Pregunta " + str(1) + ": " + micro_content['quiz'][0]['question'],
                      reply_markup=reply_keyboard)
 
     return Q2
@@ -398,9 +399,9 @@ def get_units(update, context):
     req_string = GENERAL_MANAGER_IP + "check_user/telegram/" + str(update.effective_user.id)
     re = requests.get(req_string)
     if re.status_code == 404:
-        update.message.reply_text("You must identify yourself first")
+        update.message.reply_text("Debes identificarte primero.")
         #authenticate(update, context ,re.text) #MOD estaba comentado?
-        update.message.reply_text("Introduce your username: ")
+        update.message.reply_text("Introduce tu nombre de usuario: ")
         return AUTH
     else:
         req_string = GENERAL_MANAGER_IP + "get_units/"
@@ -414,7 +415,7 @@ def get_units(update, context):
                 CallbackQueryHandler(get_unit_micro_contents, pattern='^' + u['name'] + '$'))  # Update states
 
         reply_markup = InlineKeyboardMarkup(utils.build_menu(button_list, n_cols=2))
-        update.message.reply_text("Choose a Unit of micro content: ", reply_markup=reply_markup)
+        update.message.reply_text("Elige una unidad: ", reply_markup=reply_markup)
     return MENU1
 
 
@@ -426,7 +427,7 @@ def back_to_units(update, context):
     bot = context.bot
     #revisar
     #units = requests.get('http://127.0.0.1:7000/units').json()  # Call Authoring Tool API to get the Units available
-    units = requests.get('http://192.168.0.26:7020/units').json()  # Call Authoring Tool API to get the Units available
+    units = requests.get(AUTH_TOOL_UNITS_IP).json()  # Call Authoring Tool API to get the Units available
     button_list = []
     conv_handler.states[MENU1] = []  # Clean previous states
 
@@ -440,7 +441,7 @@ def back_to_units(update, context):
     bot.send_message(
         chat_id=query.message.chat_id,
         reply_to_message_id=query.message.message_id,
-        text="Choose a Unit of micro content",
+        text="Elige una unidad",
         reply_markup=reply_markup
     )
 
@@ -466,12 +467,13 @@ def check_credentials(update, context):
     #update.message.reply_text(re.text) #DEBUG
     if re.status_code == 200:
         context.user_data['auth_check'] = True
-        update.message.reply_text("You successfully completed the identification!")
-        update.message.reply_text("Welcome to Elemend micro learning bot! \n"
-                                  "You have the following commands available:\n\n"
-                                  "/help - Show the commands available.\n"
-                                  "/menu - Show you the available Units.\n"
-                                  "/cancel - Cancel the conversation whenever you want.")
+        update.message.reply_text("Te has identificado correctamente.")
+        update.message.reply_text("¡Bienvenido/a a InclusiBot! \n"
+                                  "Comandos disponibles:\n\n"
+                                  "/help - Muestra los comandos disponibles\n"
+                                  "/menu - Muestra la lista de unidades\n"
+                                  "/cancel - Cancela la conversación")
+
     else:
         context.user_data['auth_check'] = False
      #   update.message.reply_text('llegó'+re.text[0:20]+'aquí') #DEBUG
